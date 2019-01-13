bd3f305966b069d03ceb4fab35628d27f1f18466
Daniel Sosa <daniel.sosa@mentive.com.mx>
2019-01-12 18:38:45 -0600

    feat: Implement Notification Bar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { FirebaseService } from './../../services/firebase.service';
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase/app';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -13,18 +14,26 @@ export class LoginComponent implements OnInit {
 
   public email: string;
   public password: string;
-  constructor(private _authService: FirebaseService, private router: Router) { }
+  public loading = false;
+  constructor(private _authService: FirebaseService, private router: Router, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
+    this.loading = true;
     this._authService
       .login(this.email, this.password)
       .then(res => {
         localStorage.setItem('token', this.email);
         this.router.navigate(['dashboard']);
       })
-      .catch(err => console.log(err.message));
+      .catch(err => {
+        this.loading = false;
+        this.snackBar.open('Usurio y/o contraseña incorrectos', null, {
+          duration: 2000,
+        });
+        console.log(err.message);
+      });
   }
 }
